98eb1a7952e1e9328e8a86e686079c8b79a48bf9
felipemoraless__ <felipemorales453@gmail.com>
2025-11-13 11:45:32 -0600

    Refactor main function and update imports

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+"""
+Simulador de Decaimiento Radiactivo
+====================================
+Aplicación para visualizar el decaimiento de radiofármacos en tiempo real.
+
+    @Autor: [Felipe Morales]
+        Web: [https://github.com/felipemoraless312/simulador-de-decadimiento-radioactivo-para-radiofarmacos]
+        Fecha: [28-10-2025]
+"""
+
 import customtkinter as ctk
-from modelos.radiofarmaco import radiofarmacos, escala_tiempo_dict
-from logica.simulacion import SimuladorDecaimiento
-from interfaz.gui import SimuladorGUI
-
-def main():
-    ctk.set_appearance_mode("dark")
-    ctk.set_default_color_theme("blue")    
-    root = ctk.CTk()
-
-    simulador = SimuladorDecaimiento()
-    
-    # Crear la interfaz gráfica
-    app = SimuladorGUI(root, radiofarmacos, escala_tiempo_dict, simulador)
-    
-    # Iniciar bucle principal
-    root.mainloop()
+from config.constantes import RADIOFARMACOS, ESCALAS_TIEMPO
+from modelos.simulacion import SimuladorDecaimiento
+from interfaz.gui_principal import SimuladorGUI
 
 if __name__ == "__main__":
-    main()
+    
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+    
+    root = ctk.CTk()
+    simulador = SimuladorDecaimiento()
+    app = SimuladorGUI(root, RADIOFARMACOS, ESCALAS_TIEMPO, simulador)
+    root.mainloop()
